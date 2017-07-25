d2dbfb0cb368a99f729a8b485d4640e92a63d9b2
jean-brice <jdickewu@gmail.com>
2017-07-25 12:02:01 +0000

    created initial recipe models. refs #4

--- inMyFridge/recipes/models.py
+++ inMyFridge/recipes/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Recipe (models.Model):
+    name = models.CharField(max_length=150)
+    date_published = models.DateTimeField()
+    description = models.TextField()
+    
