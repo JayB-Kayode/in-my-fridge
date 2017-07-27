e07a66a93a9b245d8faa79724c45b76be8a07417
jean-brice <jdickewu@gmail.com>
2017-07-27 00:46:54 +0000

    Activated models by running the makemigrations command

--- inMyFridge/inMyFridge/settings.py
+++ inMyFridge/inMyFridge/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'recipes.apps.recipeConfig'
+    'recipes.apps.RecipesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
