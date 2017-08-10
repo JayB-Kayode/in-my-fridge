a8fb2f3827f09974b4432df4c7b621595ecb4b78
jean-brice <jdickewu@gmail.com>
2017-08-10 15:33:28 +0000

    commented out the store.app for now because it doesn't yet exist and then migrate changes also tried to run $python manage.py syncdb! i am following this tutorial http://netai-nayek.blogspot.com/2014/08/how-to-create-or-registerlogin-and.html refs #8

--- inMyFridge/inMyFridge/settings.py
+++ inMyFridge/inMyFridge/settings.py
@@ -38,11 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.sites',
-    'store.app',
+    #'store.app',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+SITE_ID = 1
+
+
