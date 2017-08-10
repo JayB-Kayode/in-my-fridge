ffb069cd49317f1af65e2620b0f3ff2b34672c5c
jean-brice <jdickewu@gmail.com>
2017-08-10 16:02:54 +0000

    runned into errors when trying to launch server please need review refs #8

--- inMyFridge/inMyFridge/urls.py
+++ inMyFridge/inMyFridge/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-#urlpatterns = [
- #   url(r'^admin/', admin.site.urls),
-#]
-urlpatterns = patterns('',(r'^$', main_page),(r'^user/(\w+)/$', user_page),(r'^login/$', 'django.contrib.auth.views.login'),)
+urlpatterns = [
+   url(r'^admin/', admin.site.ugssrls),
+   url('',(r'^$', main_page),(r'^user/(\w+)/$', user_page),(r'^login/$', 'django.contrib.auth.views.login'),)
+]
